6cd81e5e2e1fd1a17f6b2b41b5416e8c24aa56fd
jaxangir <jaxangirr0700@gmail.com>
2024-10-15 15:00:54 +0500

    finished!

---
+++ main.js
@@ -0,0 +1,159 @@
+// 1-MISOL
+
+let sm = prompt("uzunlikni kiriting")
+sm = parseFloat (sm)
+let m = parseInt (sm/100)
+alert ( `siz kiritgan sm ${m} butun metrga teng`)
+
+// 1-MISOL TUGASHI 
+
+
+// 2-MISOL 
+
+// let kg = prompt("Kilogramni kiriting")
+// kg = parseFloat (kg)
+// let t = parseInt (kg/1000)
+// alert (`siz kiritgan sm ${t} butun tonnaga teng`)
+
+// 2-MISOL TUGASHI
+
+
+// 3-MISOL 
+// let bayt = prompt("baytlarni kiriting")
+// bayt = parseFloat (bayt)
+// let kb = parseInt (bayt/1024)
+// alert(`siz kiritgan  baytning to'liq ${kb} kb ga teng`)
+
+
+// 3-MISOL TUGASHI
+
+
+// 4-MISOL
+
+// let a = prompt("A sonini kiriting!")
+// let b = prompt ("B sonini kiriting!")
+// a = parseFloat(a)
+// b = parseFloat(b)
+// let result = parseInt(a/b)
+// alert(` ${result} `)
+
+// 4-MISOL TUGASHI
+
+
+// 5-MISOL 
+
+// let a = prompt("A sonini kiriting!")
+// let b = prompt ("B sonini kiriting!")
+// a =  parseFloat(a)
+// b =  parseFloat(b)
+// a = parseFloat(a)
+// b = parseFloat(b)
+// let qoldiq = (a%b)
+// let boluv = parseInt(a/b)
+
+// alert(` A kesmada B kesma ${qoldiq} qoldiq qolib  ${boluv} martta joylashadi `)
+
+// 5-MISOL TUGASHI
+
+
+// 6-MISOL 
+
+// let number = prompt ("ikki xonali son kiriting")
+//  number =  parseFloat(number)
+// let one = parseInt (number/10)
+// let two = parseInt (number%10)
+// alert(`
+//     o'nlik ${one} birlik  ${two}`)
+    
+// 6-MISOL TUGASHI
+
+
+// 7-MISOL
+
+// let number = prompt ("ikki xonali son kiriting")
+//  number =  parseFloat(number)
+// let one = parseInt (number/10)
+// let two = parseInt (number%10)
+// let collected = parseFloat(one+two)
+// alert(`
+//    yig'indisi ${collected}`)
+
+   // 7-MISOL TUGASHI
+
+
+ // 8-MISOL
+
+// let z = prompt("ikki xonali son kiriring!")
+// z = parseFloat (z)
+// let x = parseInt (z/10)
+// let c = z%10
+// let natija = parseFloat (x*10+c)
+// alert(`
+//     natija ${natija}`)
+
+ // 8-MISOL TUGASHI
+
+
+  // 9-MISOL
+
+// let z = prompt ("uch xonali son kiriting")
+// z = parseFloat (z)
+// let x = parseInt (z/100)
+// alert(`
+//     uch xonali sonning yuzlar xonasidagi raqam ${x} `)
+
+  // 9-MISOL TUGASHI
+
+
+//   10-MISOL 
+
+// let number = prompt("10-savol uch xonali son kiriting")
+// number =  parseFloat(number)
+// let x = parseInt (number%10)
+// let z = parseInt (number/10%10)
+// alert(`birliklar xonasidagi raqam ${x} va ikkilik xonasidagi raqam  ${z}
+//     `)
+
+    // 10-MISOL TUGASHI
+
+    // 11-MISOL
+
+    // let number = +prompt(" 11-savol uch xonali son kiriting")
+    // number =  parseFloat(number)
+    // let z = parseInt (number/100)
+    // let x = parseInt (number/10%10)
+    // let c = parseFloat (number%10)
+    // let natija = parseFloat(z+x+c)
+    // alert(`
+    //     Uch xonali sonni xar bir raqamlarining yig'indisi 
+    //     ${natija}`)
+
+
+// 11-MISOL TUGASHI
+
+
+//   12-MISOL
+
+// let number = prompt ("12-savol uch xonali son kiriting")
+// number =  parseFloat(number)
+// let q = parseInt(number/100)
+// let w = parseInt (number/10%10)
+// let e = parseInt(number%10)
+// // let natija = parseInt(e,w,q)
+// alert(`Natija ${e}${w}${q}
+//     `)
+
+// 12-MISOL TUGASHI
+
+
+//   13-MISOL
+
+// let number = +prompt("13-savol uch xonali son kiriting")
+// number = parseFloat (number)
+
+// let s = parseInt(number/100)
+// let a = parseInt(number%100*10+s)
+// alert(`
+//   Natija  ${a}`)
+
+// 13-MISOL TUGASHI
